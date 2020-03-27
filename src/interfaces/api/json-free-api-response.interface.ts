74fcb518ac95b3c2edbd292df6ce0e6a39369c48
PyColors <patweb44@gmail.com>
2020-03-27 23:10:06 +0100

    Add interfaces

---
+++ src/interfaces/api/json-free-api-response.interface.ts
@@ -0,0 +1,4 @@
+import { JsonFreeApi } from "../json-free-api.interface";
+import { ApiResponseList } from "./api-response-list";
+
+export interface JsonFreeApiResponse extends ApiResponseList<JsonFreeApi> {}
